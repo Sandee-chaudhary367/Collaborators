235cff8fa7186c0349bdb9888c8d622c092677e5
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2021-07-20 19:10:12 +0530

    deploying

--- collab-backend/db/mongoose.js
+++ collab-backend/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 //setup to connect to the database
-//mongoose.connect('process.env.MONGO_URI', {
-mongoose.connect('mongodb://127.0.0.1:27017/Collaborators', {
+mongoose.connect('process.env.MONGODB_URL', {
+// mongoose.connect('mongodb://127.0.0.1:27017/Collaborators', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 },()=>{console.log("Server started")})
+
+//
