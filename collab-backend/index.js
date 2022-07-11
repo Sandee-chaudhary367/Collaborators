3b1f26f79459b6b60bc9a4381339f00e6220464a
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2022-07-11 12:36:29 +0530

    deploying

--- collab-backend/index.js
+++ collab-backend/index.js
@@ -15,7 +15,7 @@ const auth=require("./middleware/auth");
 const user = require('./models/user');
 const { db } = require("./models/user");
 var corsOptions = {
-    origin: "http://localhost:3000"
+    origin:"https://deep-collaborators.herokuapp.com/"
 }
 app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3001;
@@ -29,6 +29,12 @@ app.use(messageRoutes);
 app.use(documentRoutes);
 app.use(logRoutes);
 
+var express = require('express');  
+var router = express.Router();
+router.get("",(req,res)=>{
+    console.log("Welcome")
+    res.send("Welcome");
+})
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
