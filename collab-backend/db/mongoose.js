a6e33e15af1d396a59d9fcd8bf131e7d5a70515b
Sandeep Chaudhary <sandee.chaudhary367@gmail.com>
2022-08-18 00:42:21 +0530

    Working Properly locally but thing need to change for deployment

--- collab-backend/db/mongoose.js
+++ collab-backend/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 //setup to connect to the database
 
-//mongoose.connect('mongodb://127.0.0.1:27017/Collaborators', {
-mongoose.connect(process.env.MONGODB_URL,{
+mongoose.connect('mongodb://127.0.0.1:27017/Collaborators', {
+
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
--- collab-backend/index.js
+++ collab-backend/index.js
@@ -16,7 +16,7 @@ const auth=require("./middleware/auth");
 const user = require('./models/user');
 const { db } = require("./models/user");
 var corsOptions = {
-    origin:"https://deep-collaborators.herokuapp.com/"
+    origin: "http://localhost:3000"
 }
 app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3001;
@@ -30,12 +30,6 @@ app.use(messageRoutes);
 app.use(documentRoutes);
 app.use(logRoutes);
 
-var express = require('express');  
-var router = express.Router();
-router.get("",(req,res)=>{
-    console.log("Welcome")
-    res.send("Welcome");
-})
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
